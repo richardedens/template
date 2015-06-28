f1e7a65fc670b9b216b761d83638ab59c8f86dc1
Richard Edens <richardedens@outlook.com>
2015-06-28 13:57:34 +0200

    - Added directive mangoForm (ported from previous framework). - Created new directive mangoImageGalary (need support for object lists in mango business layer!).

--- app/mango-angular.js
+++ app/mango-angular.js
@@ -1,208 +1,235 @@
-/*global define, mango, angular, console */
+/*global define, mango, angular, console, alert */
 (function () {
-    'use strict';
+	'use strict';
 
-    // Define main mango AngularJS app.
-    var app = angular.module('mango', ['ui.bootstrap']);
+	// Define main mango AngularJS app.
+	var app = angular.module('mango', ['ui.bootstrap']);
 
-    // Create a factory to get data from the database for the main mango controller.
-    app.factory('mangoDataSource', function ($http) {
-        var data = {
-            name: 'Mango View'
-        };
-        return {
-            fetch: function (name) {
-                return data[name];
-            }
-        };
-    });
+	// Create a factory to get data from the database for the main mango controller.
+	app.factory('mangoDataSource', function ($http) {
+		var data = {
+			name: 'Mango View'
+		};
+		return {
+			fetch: function (name) {
+				return data[name];
+			}
+		};
+	});
 
-    // Create the mango controller.
-    app.controller('mango-heart', function ($scope, mangoDataSource) {
-        $scope.name = mangoDataSource.fetch('name');
-    });
+	// Create the mango controller.
+	app.controller('mango-heart', function ($scope, mangoDataSource) {
+		$scope.name = mangoDataSource.fetch('name');
+	});
 
-    // Defining a dataview.
-    app.directive('mangoDataview', function ($compile, mangoDataSource) {
-        return {
-            restrict: 'E',
-            transclude: true,
-            scope: { },
-            controller: function ($scope, $element) {
+	// Defining a dataview.
+	app.directive('mangoDataview', function ($compile, mangoDataSource) {
+		return {
+			restrict: 'E',
+			transclude: true,
+			scope: { id: '@', action: '@' },
+			controller: function ($scope, $element) {
 
-                var scopes = [];
+				var scopes = [];
 
-                if (typeof $element.attr('id') !== 'undefined') {
-                    if (typeof $element.attr('action') !== 'undefined') {
-                        // Async load actions
-                        mango.actions.get($element.attr('action'), function (err, actions, args) {
-                            // Async execute mango busines layer actions.
-                            mango.actions.interperActions($element.attr('id'), actions, mango.hitch(this, function (result) {
-                                var attribute, scopeid;
-                                // Put the mango object values on the scope!
-                                for (attribute in result.json) {
-                                    if (result.json.hasOwnProperty(attribute)) {
-                                        for (scopeid in scopes) {
-                                            if (scopes.hasOwnProperty(scopeid)) {
-                                                scopes[scopeid][attribute] = result.json[attribute];
-                                            }
-                                        }
-                                    }
-                                }
-                                for (scopeid in scopes) {
-                                    if (scopes.hasOwnProperty(scopeid)) {
-                                        scopes[scopeid].mangoObject = result;
-                                        scopes[scopeid].$digest();
-                                    }
-                                }
-                            }));
-                        }, this);
-                    } else {
-                        mango.logger.error('The mango dataview always need an action!');
-                    }
-                } else {
-                    mango.logger.error('The mango dataview always need an ID tag!');
-                }
+				if (typeof $scope.id !== 'undefined' && $scope.id !== null && $scope.id !== '') {
+					if (typeof $scope.action !== 'undefined' && $scope.action !== null && $scope.action !== '') {
+						// Async load actions
+						mango.actions.get($scope.action, function (err, actions, args) {
+							// Async execute mango busines layer actions.
+							mango.actions.interperActions($scope.id, actions, mango.hitch(this, function (result) {
+								var attribute, scopeid;
+								// Put the mango object values on the scope!
+								for (attribute in result.json) {
+									if (result.json.hasOwnProperty(attribute)) {
+										for (scopeid in scopes) {
+											if (scopes.hasOwnProperty(scopeid)) {
+												scopes[scopeid][attribute] = result.json[attribute];
+											}
+										}
+									}
+								}
+								for (scopeid in scopes) {
+									if (scopes.hasOwnProperty(scopeid)) {
+										scopes[scopeid].mangoObject = result;
+										scopes[scopeid].$digest();
+									}
+								}
+							}));
+						}, this);
+					} else {
+						mango.logger.error('The mango dataview always need an action!');
+					}
+				} else {
+					mango.logger.error('The mango dataview always need an ID tag!');
+				}
 
-                this.addScope = function (scope) {
-                    scopes.push(scope);
-                };
+				this.addScope = function (scope) {
+					scopes.push(scope);
+				};
 
-            },
-            template: '<div class="mango-data-view" ng-transclude></div>',
-            replace: true
-        };
-    });
+			},
+			template: '<div class="mango-data-view" ng-transclude></div>',
+			replace: true
+		};
+	});
 
-    app.directive('mangoForm', function () {
-        return {
-            restrict: 'E',
-            require: '^mangoDataview',
-            transclude: true,
-            scope: {},
-            link: function (scope, element, attrs, controller) {
-                controller.addScope(scope);
-            },
-            template: '<div><span>{{firstname}}</span><div ng-transclude></div></div>',
-            replace: true
-        };
-    });
+	// Make HTML components.
+	app.directive('mangoTabs', function () {
+		return {
+			restrict: 'E',
+			require: '^mangoDataview',
+			transclude: true,
+			scope: {},
+			controller: function ($scope, $element) {
+				var panes = $scope.panes = [];
 
-    // Make HTML components.
-    app.directive('mangoTabs', function () {
-        return {
-            restrict: 'E',
-            require: '^mangoDataview',
-            transclude: true,
-            scope: {},
-            controller: function ($scope, $element) {
-                var panes = $scope.panes = [];
+				$scope.select = function (pane) {
+					angular.forEach(panes, function (pane) {
+						pane.selected = false;
+					});
+					pane.selected = true;
+				};
 
-                $scope.select = function (pane) {
-                    angular.forEach(panes, function (pane) {
-                        pane.selected = false;
-                    });
-                    pane.selected = true;
-                };
+				this.addPane = function (pane) {
+					console.log(pane);
+					if (panes.length === 0) {
+						$scope.select(pane);
+					}
+					panes.push(pane);
+				};
+			},
+			link: function (scope, element, attrs, controller) {
+				controller.addScope(scope);
+			},
+			template:   '<div class="tabbable">{{firstname}}' +
+						'<ul class="nav nav-tabs">' +
+						'<li ng-repeat="pane in panes" ng-class="{active:pane.selected}">' +
+						'<a href="" ng-click="select(pane)">{{pane.title}}</a>' +
+						'</li>' +
+						'</ul>' +
+						'<div class="tab-content" ng-transclude></div>' +
+						'</div>',
+			replace: true
+		};
+	});
 
-                this.addPane = function (pane) {
-                    console.log(pane);
-                    if (panes.length === 0) {
-                        $scope.select(pane);
-                    }
-                    panes.push(pane);
-                };
-            },
-            link: function (scope, element, attrs, controller) {
-                controller.addScope(scope);
-            },
-            template:   '<div class="tabbable">{{firstname}}' +
-                        '<ul class="nav nav-tabs">' +
-                        '<li ng-repeat="pane in panes" ng-class="{active:pane.selected}">' +
-                        '<a href="" ng-click="select(pane)">{{pane.title}}</a>' +
-                        '</li>' +
-                        '</ul>' +
-                        '<div class="tab-content" ng-transclude></div>' +
-                        '</div>',
-            replace: true
-        };
-    });
+	app.directive('mangoTab', function () {
+		return {
+			require: '^mangoTabs',
+			restrict: 'E',
+			transclude: true,
+			scope: { title: '@' },
+			link: function (scope, element, attrs, controller) {
+				controller.addPane(scope);
+			},
+			template: '<div class="tab-pane" ng-class="{active: selected}" ng-transclude></div>',
+			replace: true
+		};
+	});
 
-    app.directive('mangoTab', function () {
-        return {
-            require: '^mangoTabs',
-            restrict: 'E',
-            transclude: true,
-            scope: { title: '@' },
-            link: function (scope, element, attrs, controller) {
-                controller.addPane(scope);
-            },
-            template: '<div class="tab-pane" ng-class="{active: selected}" ng-transclude></div>',
-            replace: true
-        };
-    });
+	// Contact form
+	app.directive('mangoForm', function () {
+		return {
+			restrict: 'E',
+			transclude: true,
+			scope: { title: '@', id: '@', action: '@', table: '@' },
+			controller: function ($scope, $element) {
 
-    app.directive('mangoNav', function () {
-        return {
-            require: '^mangoTabs',
-            restrict: 'E',
-            transclude: true,
-            scope: { title: '@' },
-            link: function (scope, element, attrs, controller) {
-                controller.addPane(scope);
-            },
-            template: '<div class="tab-pane" ng-class="{active: selected}" ng-transclude></div>',
-            replace: true
-        };
-    });
+				if (typeof $scope.id !== 'undefined' && $scope.id !== null && $scope.id !== '') {
+					if (typeof $scope.table !== 'undefined' && $scope.table !== null && $scope.table !== '') {
+						if (typeof $scope.action !== 'undefined' && $scope.action !== null && $scope.action !== '') {
+							mango.client.table.get($scope.table, function (err, data) {
+								if (err) {
+									mango.logger.error('[component](' + $scope.id + '): ' + err);
+									return;
+								}
+								mango.logger.log('[component](' + $scope.id + '): got META data from the table.');
+								$scope.fields = data.fields;
+								$scope.name = data.name;
+								$scope.okButton = data.okButton;
+								$scope.cancelButton = data.cancelButton;
+								$scope.$digest();
+							}, this);
+						} else {
+							mango.logger.error('The mango formview always need an action name!');
+						}
+					} else {
+						mango.logger.error('The mango formview always need a table name!');
+					}
+				} else {
+					mango.logger.error('The mango formview always need an ID tag!');
+				}
 
-    app.directive('mangoView', function () {
-        return {
-            require: '^mangoTabs',
-            restrict: 'E',
-            transclude: true,
-            scope: { title: '@' },
-            link: function (scope, element, attrs, controller) {
-                controller.addPane(scope);
-            },
-            template: '<div class="mango-page" ng-transclude></div>',
-            replace: true
-        };
-    });
+				this.sendData = function () {
+					alert('Sending data! ' + $scope.id);
+				};
 
-    app.directive('mangoPageButton', function () {
-        return {
-            restrict: 'E',
-            transclude: true,
-            scope: { title: '@' },
-            controller: function ($scope, $element) {
-                this.openPage = function (scope, element) {
-                    var page, body;
+			},
+			link: function (scope, element, attrs, controller) {
+				scope.sendData = function () {
+					controller.sendData(scope, element);
+				};
+			},
+			template:	'<form onsubmit="return false;" method="POST">' +
+						'    <input type="hidden" name="table" value="{{table}}">' +
+						'    <input type="hidden" name="action" value="{{action}}">' +
+						'    <h1>{{name}}</h1>' +
+						'        <div ng-repeat="field in fields" class="form-group" ng-switch on="field.tag.name">' +
+						'            <label for="{{id}}_{{field.name}}">{{field.label}}</label>' +
+						'            <input ng-switch-when="input" class="form-control" id="{{id}}_{{field.name}}" type="{{field.tag.type}}" placeholder="{{field.tag.placeholder}}" />' +
+						'            <textarea ng-switch-when="textarea" class="form-control" id="{{id}}_{{field.name}}" placeholder="{{field.tag.placeholder}}" />' +
+						'        </div>' +
+						'    <div class="recaptcha {{id}}"></div>' +
+						'    <br>' +
+						'    <button id="{{id}}_submit" ng-click="sendData()" class="btn btn-primary mx-submit">{{okButton}}</button>' +
+						'</form>',
+			replace: true
+		};
+	});
+	
+	app.directive('mangoImageGalary', function () {
+		return {
+			restrict: 'E',
+			transclude: true,
+			scope: { title: '@', id: '@', action: '@' },
+			controller: function ($scope, $element) {
 
-                    page = angular.element('<div><mango-tabs><mango-tab title="tab1">content1</mango-tab><mango-tab title="tab2">content2</mango-tab></mango-tabs></div>');
-                    page.css({
-                        position: 'absolute',
-                        top: '0',
-                        bottom: '0',
-                        left: '0',
-                        right: '0',
-                        background: 'blue',
-                        zIndex: '2'
-                    });
+				if (typeof $scope.id !== 'undefined' && $scope.id !== null && $scope.id !== '') {
+					if (typeof $scope.action !== 'undefined' && $scope.action !== null && $scope.action !== '') {
+						// Async load actions
+						mango.actions.get($scope.action, function (err, actions, args) {
+							// Async execute mango busines layer actions.
+							mango.actions.interperActions($scope.id, actions, mango.hitch(this, function (result) {
+								var attribute, scopeid;
+								// Put the mango object values on the scope!
+								for (attribute in result.json) {
+									if (result.json.hasOwnProperty(attribute)) {
+										$scope[attribute] = result.json[attribute];
+									}
+								}
+								$scope.$digest();
+							}));
+						}, this);
+					} else {
+						mango.logger.error('The mango image galary always need an action!');
+					}
+				} else {
+					mango.logger.error('The mango image galary always need an ID tag!');
+				}
 
-                    body = angular.element(document).find('body').eq(0);
+				this.sendData = function () {
+				};
 
-                    body.append(page);
-                };
-            },
-            link: function (scope, element, attrs, controller) {
-                scope.openPage = function () {
-                    controller.openPage(scope, element);
-                };
-            },
-            template: '<a href="" class="mango-page btn btn-primary" ng-click="openPage()">{{title}}</a>',
-            replace: true
-        };
-    });
+			},
+			link: function (scope, element, attrs, controller) {
+				scope.sendData = function () {
+					controller.sendData(scope, element);
+				};
+			},
+			template: '<img src="{{src}}" alt="{{alt}}" width="{{width}}" height="{{height}}">',
+			replace: true
+		};
+	});
 
 }());
